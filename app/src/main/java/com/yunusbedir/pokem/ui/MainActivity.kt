7155db85f6704a7d16b60eb35e259404cc4004e6
= <=>
2022-06-27 14:15:35 +0300

    close application

--- app/src/main/java/com/yunusbedir/pokem/ui/MainActivity.kt
+++ app/src/main/java/com/yunusbedir/pokem/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
@@ -11,10 +12,7 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.get
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.yunusbedir.pokem.R
 import com.yunusbedir.pokem.databinding.ActivityMainBinding
 import com.yunusbedir.pokem.ui.permission.PermissionFragment
@@ -39,6 +37,22 @@ class MainActivity : AppCompatActivity() {
         setNavigationGraph()
         setContentView(_binding?.root)
         initObservers()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        _binding?.navView?.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_close_application -> {
+                    finishAndRemoveTask()
+                    true
+                }
+            }
+
+            NavigationUI.onNavDestinationSelected(it, navController)
+            _binding?.drawerLayout?.closeDrawer(GravityCompat.START)
+            true
+        }
     }
 
     private fun setNavigationGraph() {
